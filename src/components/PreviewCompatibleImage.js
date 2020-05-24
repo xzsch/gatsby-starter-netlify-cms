8ff0b452b9b2379b8fb3439ea7717e5af54ea786
islam <islaam.agamy@gmail.com>
2020-05-24 23:51:21 +0200

    m

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Img from "gatsby-image";
 
+//
 const PreviewCompatibleImage = ({ imageInfo }) => {
   const imageStyle = { borderRadius: "5px" };
   const { alt = "", childImageSharp, image } = imageInfo;
