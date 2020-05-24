dab616e86f4cffbb1f92bf5d44e03e7fa94bfd00
islam <islaam.agamy@gmail.com>
2020-05-25 01:53:48 +0200

    l

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,27 +16,31 @@ export const BlogPostTemplate = ({
   title,
   helmet,
   date,
+  slug,
 }) => {
   const PostContent = contentComponent || Content;
   const siteMetadata = useSiteMetadata();
-
   return (
     <section className="section">
       {helmet || ""}
       <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p className="post-description">{description}</p>
-            <p className="post-date">{date}</p>
-            <ShareButtons
-              tags={tags}
-              title={title}
-              url={`${siteMetadata.siteUrl}${slug}`}
-            />
-            <PostContent content={content} />
+            <div className="post-header__left">
+              <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+                {title}
+              </h1>
+              <p className="post-description">{description}</p>
+              <p className="post-date">{date}</p>
+            </div>
+            <div style={{ position: "relative", marginTop: "60px" }}>
+              <ShareButtons
+                tags={tags}
+                title={title}
+                url={`${siteMetadata.siteUrl}${slug}`}
+              />
+              <PostContent content={content} />
+            </div>
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
                 <h4>Tags</h4>
